def22df3d986562a17f9272d6a083f63e7296aac
varad0014 <varadbadhe0014@gmail.com>
2022-11-25 05:41:30 +0000

    commit1

--- Test.py
+++ Test.py
@@ -5,11 +5,18 @@ import unittest
 from Prog1 import summation
 
 class TestSum(unittest.TestCase):
-    def test_list_int(self):
+    def test_list_int1(self):
         """
         Test case to add two numbers
         """
         data = [20, 30]
+        result = summation(data)
+        self.assertEqual(result, 50)
+    def test_list_int2(self):
+        """
+        Test case to add two numbers
+        """
+        data = [40, 30]
         result = summation(data)
         self.assertEqual(result, 50)
 
